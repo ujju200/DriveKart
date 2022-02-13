9e36aa4beb1b401915f101f9cca1699d916ee42b
Bharat-mtr <bharatagmtr.999@gmail.com>
2022-02-13 18:16:02 +0530

    otp page added

--- src/components/House.js
+++ src/components/House.js
@@ -6,17 +6,23 @@ import Landing from "../pages/Landing.js";
 import SignupDriver from "../pages/Driver/SignupDriver.js";
 import SignupDealer from "../pages/Dealer/SignupDealer.js";
 import DriverDashboard from "../pages/Driver/DashboardDriver";
+import LoginOtpDriver from "../pages/Driver/LoginOtpDriver";
+import LoginOtpDealer from "../pages/Driver/LoginOtpDealer";
 class House extends React.Component {
   render() {
     return (
       <Router>
         <Routes>
           <Route exact path="/" element={<Landing />} />
+
           <Route path="/driver/login" exact element={<LoginDriver />} />
-          <Route path="/dealer/login" exact element={<LoginDealer />} />
+          <Route path="/driver/loginotp" exact element={<LoginOtpDriver />} />
           <Route path="/driver/signup" exact element={<SignupDriver />} />
-          <Route path="/dealer/signup" exact element={<SignupDealer />} />
           <Route path="/driver/dashboard" exact element={<DriverDashboard />} />
+
+          <Route path="/dealer/login" exact element={<LoginDealer />} />
+          <Route path="/dealer/loginotp" exact element={<LoginOtpDealer />} />
+          <Route path="/dealer/signup" exact element={<SignupDealer />} />
         </Routes>
       </Router>
     );
